15fd63815b436339c00b65ca4f6c9a5520da314a
bu7331 <122575221+bu7331@users.noreply.github.com>
2023-02-21 02:37:42 -0800

    ahh

--- studio1/script.js
+++ studio1/script.js
@@ -10,6 +10,9 @@
     const button1=document.querySelector("#button1");
     const button2=document.querySelector("#button2");
     const button3=document.querySelector("#button3");
+    const result1=document.querySelector("#result1");
+    const result2=document.querySelector("#result2");
+    const result3=document.querySelector("#result3");
 
 
     prebutton.addEventListener("click", function(event){
@@ -24,8 +27,12 @@
         const time=document.querySelector("#time").value;
         const location=document.querySelector("#location").value;
 
+        const fill1= `${yourName} was targeted by the mafia at ${time} at ${location}`;
+        result1.innerHTML=fill1;
+
         document.getElementById("screen1").className="hidden";
         document.getElementById("screen2").className="showing";  
+
     });
 
     button2.addEventListener("click", function(event){
@@ -33,6 +40,9 @@
         const activity=document.querySelector("#activity").value;
         const adverb=document.querySelector("#activity").value;
         const weapon=document.querySelector("#weapon").value;
+        const fill2= `They were ${activity} when the mafia ${adverb} pulled out a ${weapon} to make the kill`;
+
+        result2.innerHTML=fill2;
 
         document.getElementById("screen2").className="hidden";
         document.getElementById("screen3").className="showing";
@@ -44,6 +54,9 @@
         const duration=document.querySelector("#duration").value;
         const disposal=document.querySelector("#disposal").value;
         const discovery=document.querySelector("#discovery").value;
+        const fill3=`The mafia killed their target in ${duration} and disposed the body in a ${disposal} were it was discovered the following morning by ${discovery}`;
+
+        result3.innerHTML=fill3;
 
         document.getElementById("screen3").className="hidden";
         document.getElementById("theResult").className="showing";
