9a69dc95fbd7d90577050fcacaa31bc9cf152040
bu7331 <122575221+bu7331@users.noreply.github.com>
2023-03-14 16:26:25 -0800

    sdjhfs

--- studio3/script.js
+++ studio3/script.js
@@ -11,7 +11,7 @@
     const gameData={
         dice:['images/bee.png','images/dice2.png','images/dice3.png','images/dice4.png','images/dice5.png','images/dice6.png'],
         players:['player1','player2'],
-        score:['0,0'],
+        score:['0','0'],
         roll1:0,
         roll2:0,
         rollSum:0,
@@ -50,27 +50,23 @@
         gameData.rollSum=gameData.roll1 + gameData.roll2;
 
         if(gameData.rollSum==2){
-            game.innerHTML += '<p>Oh snap, snake eyes!</p>';
+            game.innerHTML += '<p>Oh no! Both froggos ate bees and died XP! </p>';
             gameData.score[gameData.index]=0;
             gameData.index ? (gameData.index=0) : (gameData.index=1);
-            setTimeout(setUpTurn,2000);
+            document.getElementById('quit').innerHTML='return';
+            // setTimeout(setUpTurn,2000);
             showCurrentScore();
         }
         else if(gameData.roll1==1 || gameData.roll2==1){
             gameData.index ? (gameData.index=0) : (gameData.index=1);
-            game.innerHTML +=`<p>Sorry, one of your rolls was a one. Switching to ${gameData.players[gameData.index]}</p>`;
+            game.innerHTML +=`<p>Oops, someone ate a bee. Switching to ${gameData.players[gameData.index]}</p>`;
             setTimeout(setUpTurn,2000);
         }
         else{
             gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
-            actionArea.innerHTML='<button id="rollagain">Roll Again</button> <button id="pass">Pass</button>';
+            actionArea.innerHTML='<button id="rollagain">Roll Again</button>';
            
             document.getElementById('rollagain').addEventListener('click',function(){
-                setUpTurn();
-            });
-
-            document.getElementById('pass').addEventListener('click',function(){
-                gameData.index ? (gameData.index=0) : (gameData.index=1);
                 setUpTurn();
             });
 
@@ -90,6 +86,6 @@
     };
 
     function showCurrentScore(){
-        score.innerHTML=`<p>The score is currently <strong>${gameData.players[0]} ${gameData.score[0]}</strong> and <strong>${gameData.players[1]} ${gameData.score[1]}</strong></p>`;
+        score.innerHTML=`<p>The score is currently <strong>${gameData.players[0]}: ${gameData.score[0]}</strong> and <strong>${gameData.players[1]}: ${gameData.score[1]}</strong></p>`;
     };
 })();
