1f5bb06a208974a8085a3673c5cfed802cde0b37
bu7331 <122575221+bu7331@users.noreply.github.com>
2023-03-23 19:51:21 -0800

    final project

--- final/finalproject/script.js
+++ final/finalproject/script.js
@@ -1,25 +1,36 @@
 (function(){
     'use strict';
     console.log('running js');
-    alert("Figure out how to access the interior of the box and find the clickable items within (not all items are clickable)");
 
     const lock=document.getElementById('lock');
+    const keychain=document.getElementById('keychain');
     const exterior=document.getElementById('exterior');
     const quilt=document.querySelector('#quiltImage');
     const kwale=document.querySelector('#kwaleImage');
     const pillow=document.querySelector('#pillowImage');
     const lighter=document.querySelector('#lighterImage');
+    const back=document.getElementById('back');
     const exit1=document.querySelector('#exit1');
     const exit2=document.querySelector('#exit2');
     const exit3=document.querySelector('#exit3');
     const exit4=document.querySelector('#exit4');
 
+    const unlock=new Audio('audio/unlock.wav');
+    const keys=new Audio('audio/keys.mp3');
+    const pop=new Audio('audio/pop.mp3');
+
     lock.addEventListener('click',function(){
+        unlock.play();
         exterior.className='hidden';
+    });
+
+    keychain.addEventListener('mouseover',function(){
+        keys.play();
     });
 
     quilt.addEventListener('click',function(){
         document.getElementById('AP').className='showing';
+        pop.play();
         console.log('hi');
     });
 
@@ -29,6 +40,7 @@
 
     kwale.addEventListener('click',function(){
         document.getElementById('ET').className='showing';
+        pop.play();
         console.log('hi');
     });
 
@@ -38,6 +50,7 @@
 
     pillow.addEventListener('click',function(){
         document.getElementById('KD').className='showing';
+        pop.play();
         console.log('hi');
     });
 
@@ -47,11 +60,17 @@
 
     lighter.addEventListener('click',function(){
         document.getElementById('NN').className='showing';
+        pop.play();
         console.log('hi');
     });
 
     exit4.addEventListener('click',function(){
         document.getElementById('NN').className='hidden';
+    });
+
+    back.addEventListener('click',function(){
+        unlock.play();
+        exterior.className='showing';
     });
 
 })();
